dc6d8f2a9937b85df6e93b58f6816dc804e3de38
kalanakt <e19198@eng.pdn.ac.lk>
2025-12-19 20:49:44 +0530

    refactor: Update PlanType enum to remove 'team' and 'enterprise' variants

---
+++ packages/db/prisma/migrations/20251219151929_plan_type_changed/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [team,enterprise] on the enum `PlanType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "PlanType_new" AS ENUM ('free', 'pro', 'premium');
+ALTER TABLE "subscription" ALTER COLUMN "plan" TYPE "PlanType_new" USING ("plan"::text::"PlanType_new");
+ALTER TYPE "PlanType" RENAME TO "PlanType_old";
+ALTER TYPE "PlanType_new" RENAME TO "PlanType";
+DROP TYPE "public"."PlanType_old";
+COMMIT;
